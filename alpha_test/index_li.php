a8923194cb5a59ee335f04941475f2eebe09a818
Stelios Vazaios <49122833+Vazeagle@users.noreply.github.com>
2020-08-29 23:13:30 +0300

    Add files via upload

---
+++ alpha_test/index_li.php
@@ -0,0 +1,63 @@
+<?php require_once 'login.php';?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Panoptikon</title>
+    <link rel="stylesheet" href="style.css">
+    
+  </head>
+  <body>
+    
+<form class="box" action="index_li.php" method="post">
+  <h1><img src="panoptikon.png" alt="logo" width="60" height="42"></h1>
+  <h1 >Welcome to Panoptikon!</h1>
+  
+       
+  <div class="btn-group">
+    <button id="su_button" onclick="signup_select()">Sign Up</button>
+    <button id="li_button" onclick="login_select()">Log In</button>
+  </div>
+
+  <script>
+  function signup_select()
+  {window.location.replace("http://localhost/test/index.php");
+  
+    if (x.style.display === "block" && y.style.display === "none") {
+      x.style.display = "none";
+      y.style.display = "block";
+      l.style.background = "#191919";
+      s.style.background = '#2ecc71';
+
+    }
+  }
+
+  function login_select()
+  {window.location.replace("http://localhost/test/index_li.php");
+  
+    if (y.style.display === "block" && x.style.display === "none") {
+        y.style.display = "none";
+        x.style.display = "block";
+        l.style.background = '#2ecc71';
+        s.style.background = '#191919';
+    }
+  }
+
+  </script>
+
+
+      <div id="login">
+
+
+        <input type="text" name = "e-mail" id="email" placeholder="E-mail">
+        <input type="password" name = "password_li" id="password" placeholder="Password" required autocomplete="off">
+        <p><a type="forgot" href="">Forgot your password? </a></p>
+        <button type="submit" name=login_btn class="button button-block">Login</button>
+  
+    </div>
+
+      </form>
+
+
+  </body>
+</html>
---
+++ alpha_test/signup.php
@@ -0,0 +1,142 @@
+<?php
+session_start();
+//οταν παταει το signup
+if (isset($_POST['signup_btn']))
+{
+//times metablhtwn apo site
+$username = $_POST['username'];
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+
+if(empty($username)) //la8os username
+    {
+        $username_error = "Please insert a valid username!\n";
+        echo $username_error;
+        $usrmane_flag = 0; 
+    }
+else
+{
+    $usrmane_flag = 1;
+}
+
+//la8os email
+if(empty($email || !filter_var($email, FILTER_VALIDATE_EMAIL)))
+{
+    $email_error = "Please insert a valid email!\n";
+    echo $email_error; 
+    $email_flag = 0;
+    //tropod gis ns tsekarei to email einai epibebaiwsh mesw confirmation mhnyma pou 8a er8ei sto email
+}
+else
+{
+    $email_flag = 1;
+}
+
+//la8os password
+if(empty($password) || strlen($password)<8 || !(preg_match('/[A-Z]/', $password)) || !(preg_match('/[a-z]/', $password)) || !(preg_match('/[0-9]/', $password)) || !(preg_match('/\W/', $password)))
+{
+    $password_error = "Please insert a valid password! At least 8 characters,  one upper-case character(A-Z), a number(0-9) and a sumbol such as @, #, $\n";
+    echo $password_error;
+    $pass_flag = 0;
+}
+else
+{
+    $pass_flag = 1;
+}
+
+
+
+//la8os confirmation password
+if(!($password == $confirm_password)) //FIX name pairnei kai thn periptwsh poy den exei grapsei kwdiko epibebaiwshs
+{
+    $conf_pass_error = "The passwords don't match up! Please check them again!\n";
+    echo $conf_pass_error;
+    $pass_conf_flag = 0;
+}
+else
+{
+    $pass_conf_flag = 1;
+}
+
+//An ola ok katallhlo flag gia na prwxvrhsoyme
+if ($usrmane_flag == 1 && $pass_flag == 1 && $email_flag == 1 && $pass_conf_flag == 1)
+{
+    $ok_flag = 1;
+}
+else
+{
+    $ok_flag = 0;
+}
+
+//AN OLA OK PROXWRAEI ME TO SIGNUP------------------------------------------------------
+if($ok_flag == 1) 
+{
+    //syndesh me local database
+    //mysql_connect("localhost","root","");
+    //mysql_select("web_project");
+
+    // Check connection
+    $dbconnect = new mysqli("localhost","root","","web_project") or die("Couldn't connect to Database web_project") ;
+
+        
+
+
+
+    //check if email already exists
+   $check_user_query = "SELECT * FROM user WHERE email = ? LIMIT 1";
+
+    $stmt1 = $dbconnect->prepare($check_user_query);
+    $stmt1->bind_param('s', $email);
+    $stmt1->execute();
+    $res = $stmt1->get_result();
+    $duplicate_count = $res->num_rows;
+
+
+    if($duplicate_count >= 1)
+    {
+        echo "A user already exists with this E-mail!";
+        $res->free_result();
+        $dbconnect->close();//close connection with database
+        exit();//maybe delete or keep?
+    }
+    else //register the user to database cause there is no duplicate
+    {
+
+        $cypherMethod='AES-256-CBC';
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
+        $userID = openssl_encrypt($email, $cypherMethod, $password, 0, $iv);
+        //$_SESSION['username'] = $username;
+        $hashed_password=password_hash($password,PASSWORD_DEFAULT);
+        $_SESSION['email'] = $email;
+        $regDate =  date ('Y-m-d H:i:s', time());
+        $role = "user";
+        $verified_usr = "no";
+        
+
+        $sign_user_query = "INSERT INTO user(userID, username, fname, lname, password, iv, email, reg_date, role, verified_user) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?)";
+        $stmt2 = $dbconnect->prepare($sign_user_query);
+        $stmt2->bind_param('ssssssssss', $userID, $username, $fname, $lname, $hashed_password, $iv, $_SESSION['email'], $regDate, $role, $verified_usr);
+        if($stmt2->execute())// mysqli_stmt_execute($stmt);
+        {
+            echo "Successful Registration to DataBase!" ."\n";
+            $stmt2->close(); 
+
+        }
+        else
+        {
+            echo "Registration to DataBase Failed!";
+            $stmt2->close(); 
+        }
+
+        exit();
+
+    }//end register the user to database
+
+
+}
+}
+
+?>
